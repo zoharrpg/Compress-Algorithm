a281b8e6fe4582e972eeb11afb33af60e7d96a93
Zohar <juj22@pitt.edu>
2021-04-02 09:25:16 -0400

    Add files via upload

--- LZWmod.java
+++ LZWmod.java
@@ -13,8 +13,24 @@ public class LZWmod {
     private static int L = 512; // number of codewords = 2^W
     private static int W = 9; // codeword width
     private static int bitcount = 0;
+    private static final int maxL =65536;
+    private static final int maxW=16;
 
-    public static void compress() {
+    public static void compress(String word) {
+
+        boolean type=false;
+
+        if(word.equals("r"))
+        {
+            BinaryStdOut.write(true);
+            type=true;
+
+        }
+        else
+        {
+            BinaryStdOut.write(false);
+
+        }
 
         TSTmod<Integer> st = new TSTmod<Integer>();
         for (int i = 0; i < R; i++)
@@ -28,6 +44,8 @@ public class LZWmod {
         current.append(c);
 
         Integer codeword = st.get(current);
+
+        boolean nolimit=true;
 
         while (!BinaryStdIn.isEmpty()) {
 
@@ -43,10 +61,14 @@ public class LZWmod {
                 BinaryStdOut.write(codeword, W);
 
                 
-                if (code == L) 
+                
+            
+                
+                if (W< maxW && code == L) 
                 {
                     L *= 2;
                     W++;
+                    
                     bitcount++;
                 }
                 
@@ -54,15 +76,36 @@ public class LZWmod {
 
                
                 // Add to symbol table if not full
+                if(code<maxL)
+                {
+                    st.put(current, code++);
+                }
+                else 
+                {
+                    
+                    
+                    if(type)
+                    {
+                    st.put(current, code++);
+                    
+                    L=512;
+                    W=9;
+                    st = new TSTmod<Integer>();
 
-               
-               
-                st.put(current, code++);
+                    for (int i = 0; i < R; i++)
+                    st.put(new StringBuilder("" + (char) i), i);
 
-               
+                    code = R + 1;
+                    }
 
-                
+                   
+
+
+
+                }
             
+
+               
 
                 // TODO: reset current
                 char last = current.charAt(current.length() - 1);
@@ -89,8 +132,11 @@ public class LZWmod {
 
     }
 
-    public static void expand() {
-        String[] st = new String[65536];
+    public static void expand(){
+        
+        boolean type = BinaryStdIn.readBoolean();
+        
+        String[] st = new String[maxL];
         int i; // next available codeword value
 
         // initialize symbol table with all 1-character strings
@@ -101,35 +147,51 @@ public class LZWmod {
         int codeword = BinaryStdIn.readInt(W);
         
         String val = st[codeword];
+        boolean nolimit = true;
 
         while (true) {
 
             
             
-            
             BinaryStdOut.write(val);
+            
+     
+        
 
-            if (i == L&& W < 16) 
+            if ( W < maxW &&i<maxL && i == L) 
             {
-                
                 W++;
                 L *= 2;
-               
-                
-              
 
             }
             
-           
-          
+            
+            if( type && W==maxW && i==maxL)
+            {
+                W=9;
+                L=512;
 
-           
-            codeword = BinaryStdIn.readInt(W);
+                st = new String[maxL];
+            for (i = 0; i < R; i++)
+                st[i] = "" + (char) i;
+                    
+                st[i++] = ""; // (unused) lookahead for EOF
+
+                    codeword = BinaryStdIn.readInt(W);
+
+                    val = st[codeword];
+
+                    BinaryStdOut.write(val);
+
+                    
+
+            }
         
 
             
+           codeword = BinaryStdIn.readInt(W);
             
-
+            
             if (codeword == R)
                 break;
             String s = st[codeword];
@@ -139,20 +201,25 @@ public class LZWmod {
             if (i < L)
             {
                 //System.err.println(i);
-                st[i++] = val + s.charAt(0);
-
+              st[i++] = val + s.charAt(0);
+            
             }
             
                 
             val = s;
-
-            
         }
+        
         BinaryStdOut.close();
     }
 
     public static void main(String[] args) {
-        if      (args[0].equals("-")) compress();
+        if (args[0].equals("-"))  
+        {
+           if(!(args[1].equals("n")||args[1].equals("r")))
+           throw new RuntimeException("Illegal command line argument");
+
+            compress(args[1]);
+        }
         else if (args[0].equals("+")) expand();
         else throw new RuntimeException("Illegal command line argument");
 
